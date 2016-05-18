2235a1e38b2b6515384bbfcad06d656e676a3f92
Valdaria <Soulitor505@gmail.com>
2016-05-18 22:22:29 +0200

    Add linux support

--- src/SaveListener.java
+++ src/SaveListener.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class SaveListener implements ActionListener {
 	Window w;
-	
+
 	SaveListener(Window w){
 		this.w = w;
 	}
@@ -12,15 +12,39 @@ public class SaveListener implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		String newPath = w.ta.getText();
 		if(!newPath.endsWith(System.getProperty("line.separator"))) {newPath = newPath+System.getProperty("line.separator");}
-		System.out.println(System.getProperty("line.separator"));
-		newPath = newPath.replace(System.getProperty("line.separator"), ";");
-		ProcessBuilder b = new ProcessBuilder("cmd.exe", "/C", "setx path \""+newPath+"\"");
-		try {
-			b.start();
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
+		this.save(newPath);
 
+	}
+
+	private String getPathValue(String oldValue){
+		String OS = System.getProperty("os.name").toLowerCase();
+		if(OS.indexOf("win") >= 0)
+			return oldValue.replace(System.getProperty("line.separator"), ";");
+		else if(OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") >= 0)
+			return oldValue.replace(System.getProperty("line.separator"), ":");
+		return "";
+	}
+
+	private void save(String p){
+		String OS = System.getProperty("os.name").toLowerCase();
+		if(OS.indexOf("win") >= 0){
+			ProcessBuilder b = new ProcessBuilder("cmd.exe", "/C", "setx path \""+this.getPathValue(p)+"\"");
+			try {
+				b.start();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
+		else if(OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") >= 0){
+			String cmd = "touch ~/.profile; echo 'PATH="+this.getPathValue(p) + "'>> ~/.profile; source ~/.bashrc";
+			System.out.println(this.getPathValue(p));
+			ProcessBuilder b = new ProcessBuilder("/bin/bash", "-c", cmd);
+			try {
+				b.start();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+		}
 	}
 
 }
--- src/Window.java
+++ src/Window.java
@@ -11,24 +11,24 @@ public class Window extends JFrame {
 		this.setSize(600, 250);
 		Container c = this.getContentPane();
 		c.setLayout(new BorderLayout());
-		
+
 		JLabel lpath = new JLabel("PATH");
 		ta = new JTextArea();
 		ta.setName("area");
 		JScrollPane sp = new JScrollPane(ta);
-		
+
 		String pathValue = System.getenv("PATH");
-		pathValue = pathValue.replace(";", System.getProperty("line.separator"));
+		pathValue = this.getPathValue(pathValue);
 		System.out.println(pathValue);
 		ta.setText(pathValue);
 		ta.setEnabled(false);
-		
+
 		//Buttons panel
 		JPanel buttons = new JPanel();
 		JPanel bCenter = new JPanel();
 		bCenter.setLayout(new FlowLayout(FlowLayout.CENTER));
 		buttons.setLayout(new GridLayout(2,1));
-		
+
 		save = new JButton("Save");
 		save.addActionListener(new SaveListener(this));
 		lock = new JButton("Unlock");
@@ -36,12 +36,21 @@ public class Window extends JFrame {
 		bCenter.add(save);
 		bCenter.add(lock);
 		buttons.add(bCenter);
-		
-		
+
+
 		c.add(lpath, BorderLayout.NORTH);
 		c.add(sp, BorderLayout.CENTER);
 		c.add(buttons, BorderLayout.SOUTH);
 		this.setVisible(true);
-		
+
+	}
+
+	private String getPathValue(String oldValue){
+		String OS = System.getProperty("os.name").toLowerCase();
+		if(OS.indexOf("win") >= 0)
+			return oldValue.replace(";", System.getProperty("line.separator"));
+		else if(OS.indexOf("nix") >= 0 || OS.indexOf("nux") >= 0 || OS.indexOf("aix") >= 0)
+			return oldValue.replace(":", System.getProperty("line.separator"));
+		return "";
 	}
 }
